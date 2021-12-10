e29091ac69270c0f98d93b86730517dca943696d
Matviienko-Artem <68031006+Matviienko-Artem@users.noreply.github.com>
2021-12-10 16:49:36 +0200

    change-funnel

--- funnel.js
+++ funnel.js
@@ -5,71 +5,71 @@ window.onload = function () {
   });
 
   // Первая воронка
-  const firstFunnel = new CanvasJS.Chart("first-funnel", {
-    animationEnabled: true,
-    animationDuration: 1500,
-    culture: "es",
+  // const firstFunnel = new CanvasJS.Chart("first-funnel", {
+  //   animationEnabled: true,
+  //   animationDuration: 1500,
+  //   culture: "es",
 
-    title: {
-      text: "",
-    },
-    data: [
-      {
-        type: "funnel",
-        indexLabelPlacement: "inside",
-        indexLabelFontColor: "white",
-        indexLabelFontWeight: "900",
-        indexLabel: "{y}",
-        toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
-        neckWidth: "65%",
-        neckHeight: 0,
-        valueRepresents: "area",
-        dataPoints: [
-          { y: 3871, label: "Vehicle sessions", color: "#1b506e" },
-          { y: 2496, label: "Engaged configurations", color: "#0f3155" },
-          { y: 1398, label: "Digital leads", color: "#1a3c60" },
-          { y: 1118, label: "Showroom trafic", color: "#245282" },
-          { y: 800, label: "Retail commercial offer", color: "#3171b1" },
-          { y: 600, label: "Retail contracts", color: "#528bb8" },
-          { y: 400, label: "Digital sales", color: "#449ac0" },
-          { y: 200, label: "Transformation rate", color: "#56bdea" },
-        ],
-      },
-    ],
-  });
+  //   title: {
+  //     text: "",
+  //   },
+  //   data: [
+  //     {
+  //       type: "funnel",
+  //       indexLabelPlacement: "inside",
+  //       indexLabelFontColor: "white",
+  //       indexLabelFontWeight: "900",
+  //       indexLabel: "{y}",
+  //       toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
+  //       neckWidth: "65%",
+  //       neckHeight: 0,
+  //       valueRepresents: "area",
+  //       dataPoints: [
+  //         { y: 3871, label: "Vehicle sessions", color: "#1b506e" },
+  //         { y: 2496, label: "Engaged configurations", color: "#0f3155" },
+  //         { y: 1398, label: "Digital leads", color: "#1a3c60" },
+  //         { y: 1118, label: "Showroom trafic", color: "#245282" },
+  //         { y: 800, label: "Retail commercial offer", color: "#3171b1" },
+  //         { y: 600, label: "Retail contracts", color: "#528bb8" },
+  //         { y: 400, label: "Digital sales", color: "#449ac0" },
+  //         { y: 200, label: "Transformation rate", color: "#56bdea" },
+  //       ],
+  //     },
+  //   ],
+  // });
 
   // Вторая воронка
-  const secondFunnel = new CanvasJS.Chart("second-funnel", {
-    animationEnabled: true,
-    animationDuration: 1000,
-    culture: "es",
-    title: {
-      text: "",
-    },
-    data: [
-      {
-        type: "funnel",
-        indexLabelPlacement: "inside",
-        indexLabelFontColor: "white",
-        indexLabelFontWeight: "900",
-        indexLabel: "{y}",
-        toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
-        neckWidth: "65%",
-        neckHeight: 0,
-        valueRepresents: "area",
-        dataPoints: [
-          { y: 3871, label: "Vehicle sessions", color: "#1b506e" },
-          { y: 2496, label: "Engaged configurations", color: "#0f3155" },
-          { y: 1398, label: "Digital leads", color: "#1a3c60" },
-          { y: 1118, label: "Showroom trafic", color: "#245282" },
-          { y: 800, label: "Retail commercial offer", color: "#3171b1" },
-          { y: 600, label: "Retail contracts", color: "#528bb8" },
-          { y: 400, label: "Digital sales", color: "#449ac0" },
-          { y: 200, label: "Transformation rate", color: "#56bdea" },
-        ],
-      },
-    ],
-  });
+  // const secondFunnel = new CanvasJS.Chart("second-funnel", {
+  //   animationEnabled: true,
+  //   animationDuration: 1000,
+  //   culture: "es",
+  //   title: {
+  //     text: "",
+  //   },
+  //   data: [
+  //     {
+  //       type: "funnel",
+  //       indexLabelPlacement: "inside",
+  //       indexLabelFontColor: "white",
+  //       indexLabelFontWeight: "900",
+  //       indexLabel: "{y}",
+  //       toolTipContent: "<b>{label}</b>: {y} <b>({percentage}%)</b>",
+  //       neckWidth: "65%",
+  //       neckHeight: 0,
+  //       valueRepresents: "area",
+  //       dataPoints: [
+  //         { y: 3871, label: "Vehicle sessions", color: "#1b506e" },
+  //         { y: 2496, label: "Engaged configurations", color: "#0f3155" },
+  //         { y: 1398, label: "Digital leads", color: "#1a3c60" },
+  //         { y: 1118, label: "Showroom trafic", color: "#245282" },
+  //         { y: 800, label: "Retail commercial offer", color: "#3171b1" },
+  //         { y: 600, label: "Retail contracts", color: "#528bb8" },
+  //         { y: 400, label: "Digital sales", color: "#449ac0" },
+  //         { y: 200, label: "Transformation rate", color: "#56bdea" },
+  //       ],
+  //     },
+  //   ],
+  // });
 
   // Первый график
   const firtChartBar = new CanvasJS.Chart("first-chart-bar", {
@@ -154,37 +154,37 @@ window.onload = function () {
   firtChartBar.render();
   secondChartBar.render();
 
-  calculatePercentageFirtsFunnel();
-  calculatePercentageSecondFunnel();
-  firstFunnel.render();
-  secondFunnel.render();
+  // calculatePercentageFirtsFunnel();
+  // calculatePercentageSecondFunnel();
+  // firstFunnel.render();
+  // secondFunnel.render();
 
-  function calculatePercentageFirtsFunnel() {
-    var dataPoint = firstFunnel.options.data[0].dataPoints;
-    var total = dataPoint[0].y;
-    for (var i = 0; i < dataPoint.length; i++) {
-      if (i == 0) {
-        firstFunnel.options.data[0].dataPoints[i].percentage = 100;
-      } else {
-        firstFunnel.options.data[0].dataPoints[i].percentage = (
-          (dataPoint[i].y / total) *
-          100
-        ).toFixed(2);
-      }
-    }
-  }
-  function calculatePercentageSecondFunnel() {
-    var dataPoint = secondFunnel.options.data[0].dataPoints;
-    var total = dataPoint[0].y;
-    for (var i = 0; i < dataPoint.length; i++) {
-      if (i == 0) {
-        secondFunnel.options.data[0].dataPoints[i].percentage = 100;
-      } else {
-        secondFunnel.options.data[0].dataPoints[i].percentage = (
-          (dataPoint[i].y / total) *
-          100
-        ).toFixed(2);
-      }
-    }
-  }
+  // function calculatePercentageFirtsFunnel() {
+  //   var dataPoint = firstFunnel.options.data[0].dataPoints;
+  //   var total = dataPoint[0].y;
+  //   for (var i = 0; i < dataPoint.length; i++) {
+  //     if (i == 0) {
+  //       firstFunnel.options.data[0].dataPoints[i].percentage = 100;
+  //     } else {
+  //       firstFunnel.options.data[0].dataPoints[i].percentage = (
+  //         (dataPoint[i].y / total) *
+  //         100
+  //       ).toFixed(2);
+  //     }
+  //   }
+  // }
+  // function calculatePercentageSecondFunnel() {
+  //   var dataPoint = secondFunnel.options.data[0].dataPoints;
+  //   var total = dataPoint[0].y;
+  //   for (var i = 0; i < dataPoint.length; i++) {
+  //     if (i == 0) {
+  //       secondFunnel.options.data[0].dataPoints[i].percentage = 100;
+  //     } else {
+  //       secondFunnel.options.data[0].dataPoints[i].percentage = (
+  //         (dataPoint[i].y / total) *
+  //         100
+  //       ).toFixed(2);
+  //     }
+  //   }
+  // }
 };
