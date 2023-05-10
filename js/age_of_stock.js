bda604d94cb3569f5c663189d0d3d8d51903f3e1
Matviienko-Artem <68031006+Matviienko-Artem@users.noreply.github.com>
2023-05-10 19:07:30 +0300

    add fixes

--- js/age_of_stock.js
+++ js/age_of_stock.js
@@ -445,6 +445,10 @@ $(document).ready(function () {
     columnDefs: [
       { className: "accent_column", targets: [10, 18, 26] },
       { className: "text_align_left", targets: [2] },
+      {
+        className: "max_width_cell",
+        targets: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+      },
     ],
     scrollX: true,
     paging: false,
